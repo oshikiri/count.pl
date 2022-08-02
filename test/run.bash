8066bdd0133cec434c09bfb29b2c944ca90efdc8
Takamasa Oshikiri <mail@oshikiri.org>
2022-08-02 22:37:10 +0900

    Add test workflow

--- test/run.bash
+++ test/run.bash
@@ -1,1 +1,18 @@
-diff <(bash test/one-to-ten/generate-fixture.sh | perl count.pl) test/one-to-ten/expected.txt
+count_failed=0
+
+run_test() {
+  test_case=$1
+
+  diff \
+    ${test_case}/expected.txt \
+    <(bash ${test_case}/generate-fixture.sh | cnt)
+
+  if [ $? -eq 0 ]; then
+    echo -e "$test_case:\tPASSED"
+  else
+    count_failed=$(($count_failed + 1))
+    echo -e "$test_case:\tFAILED"
+  fi
+}
+
+run_test one-to-ten
