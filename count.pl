95ca997ff6f59b24f93405e31a36eeebf93e5db7
Takamasa Oshikiri <mail@oshikiri.org>
2022-08-04 22:57:11 +0900

    Add options (#4)
    * Add options
    
    * Parse @ARGV

--- count.pl
+++ count.pl
@@ -1,15 +1,38 @@
 use strict;
 use warnings;
 
-my $topk        = 10;
+my $topk             = 10;
+my $show_progress    = 1;
+my $reflesh_interval = 0.5;
+
+foreach my $arg (@ARGV) {
+    if ( $arg =~ /^-(\d+)$/ ) {
+        $topk = $1;
+        next;
+    }
+
+    if ( $arg =~ /^(?:-np|--no-progress)$/ ) {
+        $show_progress = 0;
+        next;
+    }
+
+    if ( $arg =~ /^-t=(\d(?:\.\d)?)$/ ) {
+        $reflesh_interval = $1;
+        next;
+    }
+}
+
 my %counts      = ();
 my $total       = 0;
 my $last_update = time();
-my $no_progress = grep( /^--no-progress$/, @ARGV );
 
-sub showTopk {
+sub print_sorted {
+    my $n = $_[0];
+    if ( $n == -1 ) {
+        $n = keys %counts;
+    }
     my @sorted = sort { $counts{$b} <=> $counts{$a} } keys %counts;
-    foreach my $key ( splice @sorted, 0, $topk ) {
+    foreach my $key ( splice @sorted, 0, $n ) {
         print "$key: $counts{$key}\n";
     }
 }
@@ -19,7 +42,8 @@ sub clear_console {
 }
 
 sub up {
-    print "\e[${topk}A";
+    my $n = $_[0];
+    print "\e[${n}A";
 }
 
 while (<STDIN>) {
@@ -27,16 +51,16 @@ while (<STDIN>) {
     $total++;
     $counts{$_}++;
 
-    if ( !$no_progress && time() - $last_update > 0.5 ) {
+    if ( $show_progress && time() - $last_update > $reflesh_interval ) {
         $last_update = time();
 
         &clear_console();
-        &up();
-        &showTopk();
+        &up($topk);
+        &print_sorted($topk);
     }
 }
 
-if ( !$no_progress ) {
+if ($show_progress) {
     &clear_console();
 }
-&showTopk();
+&print_sorted(-1);
