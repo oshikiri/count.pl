eab60ea4d334392d48e0182553de30caf32c470e
Takamasa Oshikiri <mail@oshikiri.org>
2022-08-04 22:54:58 +0900

    Parse @ARGV

--- count.pl
+++ count.pl
@@ -2,8 +2,25 @@ use strict;
 use warnings;
 
 my $topk             = 10;
-my $show_progress    = !grep( /^--no-progress$/, @ARGV );
+my $show_progress    = 1;
 my $reflesh_interval = 0.5;
+
+foreach my $arg (@ARGV) {
+    if ( $arg =~ /^-(\d+)$/ ) {
+        $topk = $1;
+        next;
+    }
+
+    if ( $arg =~ /^(?:-np|--no-progress)$/ ) {
+        $show_progress = 0;
+        next;
+    }
+
+    if ( $arg =~ /^-t=(\d(?:\.\d)?)$/ ) {
+        $reflesh_interval = $1;
+        next;
+    }
+}
 
 my %counts      = ();
 my $total       = 0;
@@ -43,7 +60,7 @@ while (<STDIN>) {
     }
 }
 
-if ( $show_progress ) {
+if ($show_progress) {
     &clear_console();
 }
 &print_sorted(-1);
