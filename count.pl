67396e4e42493832acccc918645368820fd1af36
Takamasa Oshikiri <mail@oshikiri.org>
2022-08-13 12:54:05 +0900

    Refactor print_sorted

--- count.pl
+++ count.pl
@@ -11,10 +11,8 @@ my $clear_console    = "\033[2J";
 
 foreach my $arg (@ARGV) {
     if ( $arg =~ /^-(\d+)$/ ) {
-        $topk = $1;
-        if ( $topk == 0 ) {
-            $show_progress = 0;
-        }
+        $topk          = $1;
+        $show_progress = $topk == 0 ? 0 : 1;
         next;
     }
 }
@@ -22,41 +20,32 @@ foreach my $arg (@ARGV) {
 my %counts      = ();
 my $last_update = time();
 
-sub print_sorted {
-    my $n          = $_[0] < 0 ? scalar keys %counts : $_[0];
-    my $use_stderr = $_[1];
+sub generate_sorted_result {
+    my $n = $_[0] < 0 ? scalar keys %counts : $_[0];
 
     # NOTE: This line may be inefficient if $n << keys %counts
     my @sorted = sort { $counts{$b} <=> $counts{$a} } keys %counts;
-
     my $result = "";
     foreach my $key ( splice @sorted, 0, $n ) {
         $result .= "$key: $counts{$key}\n";
     }
-
-    if ($use_stderr) {
-        print STDERR $result;
-    }
-    else {
-        print STDOUT $result;
-    }
+    return $result;
 }
 
 while (<STDIN>) {
     chomp;
     $counts{$_}++;
-
     my $current_time = time();
     if ( $show_progress && $current_time - $last_update > $reflesh_interval ) {
         $last_update = $current_time;
         my $n = min( ( $topk, scalar keys %counts ) );
         print STDERR $clear_console;
         print STDERR "\e[${n}A";    # up n lines
-        &print_sorted( $topk, 1 );
+        print STDERR &generate_sorted_result($topk);
     }
 }
 
 if ($show_progress) {
     print STDERR $clear_console;
 }
-&print_sorted( -1, 0 );
+print STDOUT &generate_sorted_result(-1);
